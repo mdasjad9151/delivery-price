adae990eaf94fe81213cc2757e8eaac36887a420
asjad <asjad.ajic@mail.com>
2024-02-26 23:23:44 +0530

    gi

--- price/views.py
+++ price/views.py
@@ -14,31 +14,31 @@ from .models import Organization,Item,Pricing
 # @api_view(['POST'])
 # @csrf_exempt
 def calculate_delivery_price(request):
-    if request.method == 'GET':
-        json_data =  request.body
-        stream = io.BytesIO(json_data)
-        python_data = JSONParser().parse(stream=stream)
-        serializer = PricingRequestSerializer(data=python_data)
-        if serializer.is_valid():
-            zone = serializer.validated_data['zone']
-            organization_id = serializer.validated_data['organization_id']
-            total_distance = serializer.validated_data['total_distance']
-            item_type = serializer.validated_data['item_type']
+    # if request.method == 'GET':
+    #     json_data =  request.body
+    #     stream = io.BytesIO(json_data)
+    #     python_data = JSONParser().parse(stream=stream)
+    #     serializer = PricingRequestSerializer(data=python_data)
+    #     if serializer.is_valid():
+    #         zone = serializer.validated_data['zone']
+    #         organization_id = serializer.validated_data['organization_id']
+    #         total_distance = serializer.validated_data['total_distance']
+    #         item_type = serializer.validated_data['item_type']
             
 
-            total_price = PricingService.calculate_total_price(zone, organization_id, total_distance, item_type)
-            # total_price = 20.0
-            if total_price is not None:
-                response_data = {'total_price': total_price}
-                response_serializer = PricingResponseSerializer(response_data)
-                return JsonResponse(response_serializer.data)
-            else:
-                return JsonResponse({'error': 'Pricing information not found for the given parameters'})
-        else:
-            return JsonResponse(serializer.errors)
-    else:
-        return JsonResponse({'error': 'Only POST requests are allowed'})
-    # return HttpResponse("HII")
+    #         total_price = PricingService.calculate_total_price(zone, organization_id, total_distance, item_type)
+    #         # total_price = 20.0
+    #         if total_price is not None:
+    #             response_data = {'total_price': total_price}
+    #             response_serializer = PricingResponseSerializer(response_data)
+    #             return JsonResponse(response_serializer.data)
+    #         else:
+    #             return JsonResponse({'error': 'Pricing information not found for the given parameters'})
+    #     else:
+    #         return JsonResponse(serializer.errors)
+    # else:
+    #     return JsonResponse({'error': 'Only POST requests are allowed'})
+    return HttpResponse("HII")
 
 
 
